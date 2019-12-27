139b98f7002eddbfaf1ec01d16daaf694e5ec692
Zachary Gill <ZGorlock@gmail.com>
2019-12-26 19:39:41 -0500

    Updated Dictionary

---
+++ src/tool/DictionaryProducer.java
@@ -0,0 +1,42 @@
+/*
+ * File:    DictionaryProducer.java
+ * Package: tool
+ * Author:  Zachary Gill
+ */
+
+package tool;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+import utility.Filesystem;
+import utility.StringUtility;
+
+/**
+ * Produces the dictionary file.
+ */
+public class DictionaryProducer {
+    
+    //Main Method
+    
+    /**
+     * The main method.
+     *
+     * @param args Arguments to the main method.
+     */
+    public static void main(String[] args) {
+        Filesystem.writeLines(new File("etc/dicts/dict.txt"), 
+                Filesystem.getFiles(new File("etc/dicts/original")).stream()
+                          .map(Filesystem::readLines)
+                          .flatMap(Collection::stream)
+                          .filter(StringUtility::isAlphabetic)
+                          .map(String::toLowerCase)
+                          .distinct()
+                          .sorted(Comparator.naturalOrder())
+                          .collect(Collectors.toList())
+        );
+    }
+    
+}
